4c707d4dc44fa1d43cc946969908e78131447430
Jannat <80195995+ferdousz@users.noreply.github.com>
2021-10-20 22:11:10 +0600

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../Login/hooks/useAuth';
+
+
 
 const Register = () => {
+    const { hanldeEmail, hanldePassword, handleUserRegister, signInUsingGoogle } = useAuth();
+    const location = useLocation();
+  const history = useHistory();
+  const redirect_url = location?.state?.from || "/loginandregistration";
+
+
+  const handleGoogleLogin = () => {
+      signInUsingGoogle()
+          .then(result => {
+              history.push(redirect_url);
+          })
+  }
+    
+
     return (
         <div className="login-form">
             <div>
                 <h2>Register: Create Account</h2>
-                <form onSubmit="">
-                    <input className="mt-2" type="email" name="" id="" placeholder="Your Email" />
+                <form onSubmit={handleUserRegister } >
+                    <input onChange={hanldeEmail } className="mt-2" type="email" name="" id="" placeholder="Your Email" />
                     <br />
-                    <input className="mt-2" type="password" name="" id="" placeholder="Your Password" />
+                    <input onChange={hanldePassword} className="mt-2" type="password" name="" id="" placeholder="Your Password" />
                     <br />
-                    <input className="mt-2" type="password" name="" id="" placeholder="Re-enter Password" />
-                    <br />
-                    <input className="mt-3 btn btn-outline-dark" type="submit" value="Submit" />
+
+                    <input
+                className="mt-3 w-50 btn btn-success m-auto"
+                type="submit"
+                value="Register"
+              />
                 </form>
                 <p>Already have an account? <Link to="/logpage">Login</Link></p>
                 <div>----------or-------------</div>
-                <button className="btn btn-outline-dark">Google Sign In</button>
+                <button onClick={handleGoogleLogin} className="btn btn-outline-dark">Google Sign In</button>
             </div>
         </div>
     );
